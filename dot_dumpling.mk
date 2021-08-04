48322eaae147b5a32588f3faec24545149c6fde6
Karthik N <45189728+karthik990@users.noreply.github.com>
2021-08-04 06:50:33 +0530

    dumpling: update FP and desc to latest redfin

--- dot_dumpling.mk
+++ dot_dumpling.mk
@@ -45,8 +45,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus5T
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-key
 
 TARGET_VENDOR := oneplus
