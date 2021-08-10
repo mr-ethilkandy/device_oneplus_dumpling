25e574011eaf71783966fa854bcccfd5f2d5cabe
Karthik N <45189728+karthik990@users.noreply.github.com>
2021-08-10 09:13:19 +0530

    Dumpling: fix derps

--- dot_dumpling.mk
+++ dot_dumpling.mk
@@ -47,6 +47,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-key
-
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
 TARGET_VENDOR := oneplus
